460ac5b81cb824767096015a08d9584e01a681c3
alpha <827130441@qq.com>
2018-05-08 12:03:35 +0800

    添加代码

--- code/main.js
+++ code/main.js
@@ -143,31 +143,79 @@
 //     console.log(a, b)
 // })(3, 4)
 
-(() => {
-    // 验证任务队列有没有插队的行为
-    // 先让主线程生成两个setTimeout和两个process.nextTick
-    setTimeout(() => {
-        console.log('timeout 1')
-        process.nextTick(() => {
-            console.log('process 3')
-        })
-        setTimeout(() => {
-            console.log('timeout 3')
-        })
-    });
-    setTimeout(() => {
-        console.log('timeout 2')
-    });
-    process.nextTick(() => {
-        console.log('process 1')
-        process.nextTick(() => {
-            console.log('process 4')
-        })
-        setTimeout(() => {
-            console.log('timeout 4')
-        })
-    });
-    process.nextTick(() => {
-        console.log('process 2')
-    });
-})()
+// (() => {
+//     // 验证任务队列有没有插队的行为
+//     // 先让主线程生成两个setTimeout和两个process.nextTick
+//     setTimeout(() => {
+//         console.log('timeout 1')
+//         process.nextTick(() => {
+//             console.log('process 3')
+//         })
+//         setTimeout(() => {
+//             console.log('timeout 3')
+//         })
+//     });
+//     setTimeout(() => {
+//         console.log('timeout 2')
+//     });
+//     process.nextTick(() => {
+//         console.log('process 1')
+//         process.nextTick(() => {
+//             console.log('process 4')
+//         })
+//         setTimeout(() => {
+//             console.log('timeout 4')
+//         })
+//     });
+//     process.nextTick(() => {
+//         console.log('process 2')
+//     });
+// })()
+
+
+
+// 验证箭头函数的this指向，在浏览器环境下测试通过
+// var x = 'windows';
+// function fun1() {
+//     var fun2 = () => {
+//         console.log(this.x)
+//     }
+//     fun2()
+// }
+// fun1();
+
+// var obj = {
+//     x: 'obj',
+//     showX: fun1
+// }
+// obj.showX()
+
+// function constructorDemo(x){
+//     this.x = x
+//     this.showX = () => {
+//         console.log(this.x)
+//     }
+// }
+// new constructorDemo('new').showX()
+
+
+// 验证bind
+// function list() {
+//     console.log(this)
+//     return Array.prototype.slice.call(arguments);
+// }
+
+// var f1 = list.bind(undefined,37);
+// var f2 = f1.bind(undefined, 1, 2, 3);
+
+// console.log(f1(4))
+// console.log(f2(4,5))
+
+// 验证暂时性死区
+var x = 0;
+function f(x = 2) {
+    console.log(x)
+    console.log(y)
+    let y = 1;
+}
+f()
